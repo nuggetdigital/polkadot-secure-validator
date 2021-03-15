7f3802180d8fa7f78bd7c1865ef1338d4fc1154d
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-03-15 17:34:37 +0100

    fix: dup bucket name

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "terraform-remote-state"
+    bucket = "zyzx-terraform-remote-state"
     key    = "zyzx.tfstate"
     region = "us-east-1"
   }
